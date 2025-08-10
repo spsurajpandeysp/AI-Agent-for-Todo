cd73d00eb95a214b1ab549bb8e654931ab840a4b
Suraj Pandey <surajpandey7493@gmail.com>
2025-08-10 22:48:33 +0530

    postgres connection

---
+++ db/schema.js
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar,text,timestamp } from "drizzle-orm/pg-core";
+
+export const todosTable= pgTable("todos", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  todo: text().notNull(),
+  createdAt:timestamp('created_at').defaultNow(),
+  updatedAt:timestamp('updated_at').$onUpdate(()=>new Date()),
+});
---
+++ drizzle/0001_reflective_shiver_man.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "todos" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "todos_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"todo" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+DROP TABLE "users" CASCADE;
